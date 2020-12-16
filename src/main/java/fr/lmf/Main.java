62c89a151e1c82c8afb4bcae63c960d068ff40f2
Mysterious_Dev <mysteriousdevs@gmail.com>
2020-12-16 18:00:12 +0100

    Initial commit

---
+++ src/main/java/fr/lmf/Main.java
@@ -0,0 +1,46 @@
+package fr.lmf;
+
+import fr.lmf.init.ItemInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+
+@Mod(Main.MODID)
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "item_color_tutorial";
+
+    public Main() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ItemInit.ITEMS.register(bus);
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        Minecraft.getInstance().getItemColors().register((stack, color) -> {
+            return stack.getTag() != null && stack.getTag().contains("Color") ? stack.getTag().getInt("Color") : 0x13421772;
+        }, ItemInit.COLOR_ITEM.get());
+    }
+}
